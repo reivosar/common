e8303e9fb10808d56a76387ee85fcc75afb301c5
reivosar <reivosar@yahoo.co.jp>
2020-11-03 20:49:27 +0900

    serialize追加

--- java-common/src/main/java/reivosar/common/domain/model/Model.java
+++ java-common/src/main/java/reivosar/common/domain/model/Model.java
@@ -1,11 +1,13 @@
 package reivosar.common.domain.model;
 
+import java.io.Serializable;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-abstract class Model
+abstract class Model implements Serializable
 {
 	@Override
 	public boolean equals(Object obj) {
