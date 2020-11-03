831ce0959ec2e75e34ba277aa1ee43ca19ce41b5
reivosar <reivosar@yahoo.co.jp>
2020-11-03 13:39:56 +0900

    新規追加

---
+++ java-common/src/main/java/reivosar/common/domain/model/time/DateTime.java
@@ -0,0 +1,23 @@
+package reivosar.common.domain.model.time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import reivosar.common.domain.model.ValueObject;
+
+public class DateTime extends ValueObject<DateTime>
+{
+	private final LocalDateTime value;
+
+	public DateTime(LocalDateTime value) {
+		this.value = value;
+	}
+
+	public static DateTime now() {
+		return new DateTime(LocalDateTime.now());
+	}
+
+	public String formattedString(String format) {
+		return value.format(DateTimeFormatter.ofPattern(format));
+	}
+}
