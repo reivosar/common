fc1a3a0cee89414a95b9719fee50adbefaaff091
reivosar <reivosar@yahoo.co.jp>
2020-11-08 17:18:44 +0900

    ファクトリメソッド追加

--- java-common/src/main/java/reivosar/common/domain/model/message/MessageBody.java
+++ java-common/src/main/java/reivosar/common/domain/model/message/MessageBody.java
@@ -7,11 +7,23 @@ public class MessageBody extends ValueObject<MessageBody> {
 	final MessageBodyType messageBodyType;
 	final MessagePayload messagePayload;
 
-	public MessageBody(
+	private MessageBody(
 		MessageBodyType messageBodyType,
 		MessagePayload messagePayload)
 	{
 		this.messageBodyType = messageBodyType;
 		this.messagePayload  = messagePayload;
+	}
+
+	public static MessageBody of(MessageBodyType type, Object value) {
+		return new MessageBody(type, new MessagePayload(value));
+	}
+
+	public static MessageBody of(String text) {
+		return new MessageBody(MessageBodyType.TEXT, new MessagePayload(text));
+	}
+
+	public static MessageBody of(Class<?> clazz) {
+		return new MessageBody(MessageBodyType.CLASS, new MessagePayload(clazz));
 	}
 }
