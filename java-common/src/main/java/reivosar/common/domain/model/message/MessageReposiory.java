5ec2694f0af26a946e5c723924f06b3441fc9bf5
reivosar <nextcareerplus@gmail.com>
2020-11-29 01:16:50 +0900

    不要なインポート削除

--- java-common/src/main/java/reivosar/common/domain/model/message/MessageReposiory.java
+++ java-common/src/main/java/reivosar/common/domain/model/message/MessageReposiory.java
@@ -1,8 +1,5 @@
 package reivosar.common.domain.model.message;
 
-import reivosar.common.domain.model.message.Message;
-import reivosar.common.domain.model.message.MessageId;
-
 public interface MessageReposiory
 {
 	MessageId generateId();
